52be16d2092737758fd5a28913a307215e6fb1ac
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2015-08-29 06:11:32 +0900

    611

--- src/GP2Y0A21YK.cpp
+++ src/GP2Y0A21YK.cpp
@@ -1,9 +1,22 @@
+/*!
+ * @file  GP2Y0A21YK.cpp
+ * @brief 距離センサGP2Y0A21YKの制御関連のクラス
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 #include "GP2Y0A21YK.h"
 
+
+/**
+*@brief 距離センサGP2Y0A21YKの制御関連のクラスのコンストラクタ
+* @param response 初期化成功でMRAA_SUCCESS、それ以外は失敗
+* @param pin アナログピン番号(GP2Y0A21YKの通信線と接続)
+* @param r IIRフィルタの係数
+*/
 GP2Y0A21YK::GP2Y0A21YK(mraa_result_t &response, int pin, double r) {
 	m_pin = pin;
 	a = new mraa::Aio(pin);
@@ -23,6 +36,9 @@ GP2Y0A21YK::GP2Y0A21YK(mraa_result_t &response, int pin, double r) {
 	_r = r;
 }
 
+/**
+*@brief 距離センサGP2Y0A21YKの制御関連のクラスのデストラクタ
+*/
 GP2Y0A21YK::~GP2Y0A21YK() {
 	if(a)
 	{
@@ -30,6 +46,10 @@ GP2Y0A21YK::~GP2Y0A21YK() {
 	}
 }
 
+/**
+*@brief 計測した距離取得
+* @return 距離
+*/
 double GP2Y0A21YK::getDistanceData() {
 	float vol = a->readFloat();
 	double distance = voltage2distance(vol*5)/100;
@@ -37,7 +57,11 @@ double GP2Y0A21YK::getDistanceData() {
 	return distance;
 }
 
-
+/**
+*@brief アナログピン再設定
+* @param pin ピン番号
+* @return 成功でMRAA_SUCCESS、それ以外は失敗
+*/
 mraa_result_t GP2Y0A21YK::setPinNum(int pin)
 {
 	if(pin != m_pin)
@@ -55,11 +79,19 @@ mraa_result_t GP2Y0A21YK::setPinNum(int pin)
 	return MRAA_SUCCESS;
 }
 
+/**
+*@brief フィルタ係数再設定
+* @param r 係数
+*/
 void GP2Y0A21YK::setCoefficient(double r)
 {
 	_r = r;
 }
 
+/**
+*@brief 計測した距離取得(フィルタ処理有り)
+* @return 距離
+*/
 double GP2Y0A21YK::getDistance() {
 	
 
@@ -68,45 +100,50 @@ double GP2Y0A21YK::getDistance() {
 	return lastDistance;
 }
 
-
+/**
+*@brief 電圧値から距離に変換
+*http://nekosan0.bake-neko.net/connection_ir_measure.htmlのデータを使用
+* @param dVolt 電圧値
+* @return 距離
+*/
 double GP2Y0A21YK::voltage2distance(double dVolt) {
   
   	double dDist;
   
   
 	if(dVolt < 0.384321){
-	    dDist = 999.0;                        /*out of range*/
+	    dDist = 999.0;
 
 	  }else if(dVolt < 0.430210){
-	    dDist = dVolt * (-217.916667) + 163.750000;      /*80 - 70*/
+	    dDist = dVolt * (-217.916667) + 163.750000;
 	    
 	  }else if (dVolt < 0.493308){
-	    dDist = dVolt * (-158.484848) + 138.181818;    /*70 - 60*/
+	    dDist = dVolt * (-158.484848) + 138.181818;
 	      
 	  }else if (dVolt < 0.590822){
-	    dDist = dVolt * (-102.549020) + 110.588235;  /*60 - 50*/
+	    dDist = dVolt * (-102.549020) + 110.588235;
 	        
 	  }else if (dVolt < 0.722753){
-	    dDist = dVolt * (-75.797101) + 94.782609;  /*50 - 40*/
+	    dDist = dVolt * (-75.797101) + 94.782609;
 	          
 	  }else if (dVolt < 0.894837){
-	    dDist = dVolt * (-58.111111) + 82.000000;  /*40 - 30*/
+	    dDist = dVolt * (-58.111111) + 82.000000;
 	            
 	  }else if (dVolt < 1.284895){
-	    dDist = dVolt * (-25.637255) + 52.941176;  /*30 - 20*/
+	    dDist = dVolt * (-25.637255) + 52.941176;
 	              
 	  }else if (dVolt < 1.623327){
-	    dDist = dVolt * (-14.774011) + 38.983051;  /*20 - 15*/
+	    dDist = dVolt * (-14.774011) + 38.983051;
 	                
 	  }else if (dVolt < 2.294455){
-	    dDist = dVolt * (-7.450142) + 27.094017;  /*15 - 10*/
+	    dDist = dVolt * (-7.450142) + 27.094017;
 	                  
 	  }else if (dVolt < 3.131931){
-	    dDist = dVolt * (-5.970320) + 23.698630;  /*10 - 5*/
+	    dDist = dVolt * (-5.970320) + 23.698630;
 	  }
 
 	  else{
-	    dDist = 0.0;                              /*error*/
+	    dDist = 0.0;
 	}
 
 	return dDist;
